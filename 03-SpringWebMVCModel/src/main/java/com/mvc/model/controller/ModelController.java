72aa6bcf6b2bd0df43a45a46a0a38139773878e9
Dev Pattanaik <pattanaikd1998@gmail.com>
2024-11-05 08:11:02 +0530

    mvc thymeleaf

---
+++ 03-SpringWebMVCModel/src/main/java/com/mvc/model/controller/ModelController.java
@@ -0,0 +1,13 @@
+package com.mvc.model.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ModelController {
+	
+	@GetMapping("/home")
+	public String getUserHome() {
+		return "UserHome";
+	}
+}
